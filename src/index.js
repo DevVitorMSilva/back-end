371fbf8c64eeee6e3766692cec655b833e2cb9c9
Vitor Silva <vitormarcos2005@hotmail.com>
2024-08-12 21:02:12 -0300

    Resolvendo exercício DoDev #1 back-end

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
-app.get('/', (req, res) => {
-    res.send("app GET")
-})
-
-app.post('/', (req, res) => {
-    res.send("app POST")
-})
-
-app.delete('/', (req, res) => {
-    res.send("app DELETE")
-}) 
+app.use(express.json())
 
 app.listen(PORT, () =>{
-     console.log(`App online na porta ${PORT}`)
+    console.log(`App online na porta ${PORT}`)
 })
+
+app.get('/usuarios', (req, res) => {
+    console.log("API get todos")
+    res.status(200).send("todos os usuários")
+})
+
+app.get('/usuarios/:id', (req, res) => {
+    console.log("API get by id")
+    res.status(200).send(req.params.id)
+})
+
+app.post('/usuarios', (req, res) => {
+    console.log("API post usuário")
+    res.status(200).send(req.body)
+})
+
+app.put('/usuarios/:id', (req, res) => {
+    console.log("API put usuário")
+    res.status(200).send(req.params.id)
+})
+
+app.delete('/usuarios/:id', (req, res) => {
+    console.log("API delete usuários")
+    res.status(200).send(req.params.id)
+}) 
+
+
