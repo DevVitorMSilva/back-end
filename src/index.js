4f5b3190d09ae148269566f536b3c5ac87e2d5e3
Vitor Silva <vitormarcos2005@hotmail.com>
2024-08-14 20:03:03 -0300

    Simulando banco de dados

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = 3001
 
+const alunos = []
+
 app.use(express.json())
 
 app.listen(PORT, () =>{
@@ -9,28 +11,32 @@ app.listen(PORT, () =>{
 })
 
 app.get('/usuarios', (req, res) => {
-    console.log("API get todos")
-    res.status(200).send("todos os usuários")
+    res.status(200).send({alunos :alunos})
 })
 
 app.get('/usuarios/:id', (req, res) => {
-    console.log("API get by id")
-    res.status(200).send(req.params.id)
+    const aluno = alunos.find(x => x.id == req.params.id)
+    res.status(200).send(aluno)
 })
 
 app.post('/usuarios', (req, res) => {
     console.log("API post usuário")
+    alunos.push(req.body)
     res.status(200).send(req.body)
 })
 
 app.put('/usuarios/:id', (req, res) => {
-    console.log("API put usuário")
-    res.status(200).send(req.params.id)
+    const aluno = alunos.find(x => x.id == req.params.id)
+    const idAluno = alunos.indexOf(aluno)
+    alunos[idAluno] = req.body
+    res.status(200).send("aluno atualiazdo com sucesso")
 })
 
 app.delete('/usuarios/:id', (req, res) => {
-    console.log("API delete usuários")
-    res.status(200).send(req.params.id)
+    const aluno = alunos.find(x => x.id == req.params.id)
+    const idAluno = alunos.indexOf(aluno)
+    alunos.splice(idAluno, 1)
+    res.status(200).send("aluno deletado com sucesso")
 }) 
 
 
