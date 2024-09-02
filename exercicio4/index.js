bd1a6c770b447e1a67c74d07b9c8f56267b0d77f
Vitor Silva <vitormarcos2005@hotmail.com>
2024-09-02 19:16:47 -0300

    Trocando o nome na URL

--- exercicio4/index.js
+++ exercicio4/index.js
@@ -10,23 +10,23 @@ app.listen(PORT, () => {
     console.log(`Servidor ligado na porta ${PORT}`)
 })
 
-app.get('/carros', async (req, res) => {
+app.get('/alunos', async (req, res) => {
     res.status(200).send(await dbcontext.list())
 })
 
-app.get('/carros/:id', async(req, res) => {
+app.get('/alunos/:id', async(req, res) => {
     res.status(200).send(await dbcontext.get(req.params.id))
 })
 
-app.post('/carros', async (req, res) => {
+app.post('/alunos', async (req, res) => {
     res.status(200).send(await dbcontext.insert(req.body))
 })
 
-app.put('/carros/:id', async(req, res) => {
+app.put('/alunos/:id', async(req, res) => {
     res.status(200).send(await dbcontext.update(req.body, req.params.id))
 })
 
-app.delete('/carros/:id', async(req, res) => {
+app.delete('/alunos/:id', async(req, res) => {
     await dbcontext.del(req.params.id)
     res.status(200).send("Carro deletado com sucesso!")
 })
